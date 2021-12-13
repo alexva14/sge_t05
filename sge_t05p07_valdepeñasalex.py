4ff6dc11d26bf70281e72a1ab09978a1eba34183
alexva14 <alex.membry@gmail.com>
2021-12-13 11:22:35 +0100

    Ejerciciso 7 y 22

--- sge_t05p07_valdepeñasalex.py
+++ sge_t05p07_valdepeñasalex.py
@@ -1,7 +1,15 @@
 tiempo=""
-segundos=0
+segundos1=0
+segundos2=0
 
 print("Intoduce una hora en el sigueinte formato hh:mm:ss")
 tiempo= input().split(":")
-segundos= (int(tiempo[0])*60*60)+(int(tiempo[1])*60)+(int(tiempo[2]))
-print(str(segundos)+ " segundos")
+segundos1= (int(tiempo[0])*60*60)+(int(tiempo[1])*60)+(int(tiempo[2]))
+print(str(segundos1)+ " segundos")
+
+print("Intoduce una hora en el sigueinte formato hh:mm:ss")
+tiempo= input().split(":")
+segundos2= (int(tiempo[0])*60*60)+(int(tiempo[1])*60)+(int(tiempo[2]))
+print(str(segundos2)+ " segundos")
+
+print("La diferencia en segundos entre los dos tiempos es: " + str(abs(segundos1-segundos2))+ " segundos")
--- sge_t05p22_valdepeñasalex.py
+++ sge_t05p22_valdepeñasalex.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 
 palabra=""
 lista=[]
+
+#alamcenamos todas las palabras en una lista
 while palabra !="salir":
     print("Escribe un fabricante de dispositivos moviles")
     palabra=input()
@@ -13,4 +15,18 @@ aux = defaultdict(list)
 for index, item in enumerate(lista):
     aux[item].append(index)
 result = {item: indexs for item, indexs in aux.items() if len(indexs) > 1}
-print(result)
+
+#comprobamos si el diccionario
+if bool(result):
+    print("Hay elementos reptidos")
+    print(result)
+else:
+    print("No hay elementos repetidos en esta lista")
+
+listaordenada= sorted(lista)
+
+if lista==listaordenada:
+    print("Lista ordenada")
+else:
+    print("Lista no ordenada alfabeticamente")
+    
